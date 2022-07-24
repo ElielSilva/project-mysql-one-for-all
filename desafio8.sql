492289a744037d656133bc2b1d75ec6220a33a36
eliel <silvaeliel940@gmail.com>
2022-07-24 12:54:58 -0300

    fix: req 8

--- desafio8.sql
+++ desafio8.sql
@@ -2,4 +2,6 @@ SELECT art.full_name AS artista , alb.title_album AS album
 FROM SpotifyClone.album AS alb
 INNER JOIN SpotifyClone.artist AS art
 ON alb.artist_id=art.artist_id
+WHERE art.full_name = 'Walter Phoenix'
+ORDER BY title_album
 ;
